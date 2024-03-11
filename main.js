23b8c7b416de3481d7dafa20f1a4b887e62323c8
MatteoStrazzera <mattestrazz@gmail.com>
2024-03-11 12:57:22 +0100

    add h1 and img with vue.js

--- main.js
+++ main.js
@@ -9,3 +9,15 @@ Bonus:
 Aggiungere alla pagina un’immagine, presa anch’essa da un data.
 
 */
+
+
+const { createApp } = Vue
+
+  createApp({
+    data() {
+      return {
+        message: 'Never Gonna Give You Up',
+        image: 'https://i.pinimg.com/originals/89/5c/e7/895ce751ba0379700381d17a67086931.gif'
+      }
+    }
+  }).mount('#app')
